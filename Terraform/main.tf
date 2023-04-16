b24196dd99e9b21628c03d91d90cd73a307b88b3
Joe Richardson <joeyrichardson96@gmail.com>
2023-04-16 20:23:16 +0100

    gateway

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -169,3 +169,26 @@ resource "aws_lb_listener" "operation_stacked_listener" {
 output "alb_dns_name" {
   value = aws_lb.operation_stacked_alb.dns_name
 }
+
+resource "aws_internet_gateway" "operation_stacked_igw" {
+  vpc_id = aws_vpc.operation_stacked_vpc.id
+}
+
+resource "aws_route_table" "operation_stacked_route_table" {
+  vpc_id = aws_vpc.operation_stacked_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.operation_stacked_igw.id
+  }
+}
+
+resource "aws_route_table_association" "operation_stacked_subnet_1_association" {
+  subnet_id      = aws_subnet.operation_stacked_subnet_1.id
+  route_table_id = aws_route_table.operation_stacked_route_table.id
+}
+
+resource "aws_route_table_association" "operation_stacked_subnet_2_association" {
+  subnet_id      = aws_subnet.operation_stacked_subnet_2.id
+  route_table_id = aws_route_table.operation_stacked_route_table.id
+}
