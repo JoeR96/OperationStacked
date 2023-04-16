4dfa0c6fb653ce1700ebd31fd74e76f2166d8d44
Joe Richardson <joeyrichardson96@gmail.com>
2023-04-16 02:26:35 +0100

    vpc

--- Terraform/rds.tf
+++ Terraform/rds.tf
@@ -88,3 +88,10 @@ resource "aws_db_subnet_group" "operationstacked_db_subnet_group" {
     Name = "OperationStacked DB Subnet Group"
   }
 }
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "example-vpc"
+  }
+}
