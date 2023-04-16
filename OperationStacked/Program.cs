0df0273b6d3b74863e7481bcd55d60402fb9e342
Joe Richardson <joeyrichardson96@gmail.com>
2023-04-16 23:10:38 +0100

    test

--- OperationStacked/Program.cs
+++ OperationStacked/Program.cs
@@ -8,7 +8,6 @@ using Amazon.SimpleSystemsManagement.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
-var connectionString = Environment.GetEnvironmentVariable("CONNECTION_STRING");
 
 var tokenOptions = config.GetSection("TokenOptions").Get<TokenOptions>();
 var signingConfigurations = new SigningConfigurations(tokenOptions.Secret);
@@ -35,40 +34,50 @@ builder.Services.AddCors(options =>
     )
 );
 
-builder.Services.AddDbContext<OperationStackedContext>(options =>
+
+
+await using var app = builder.Build();
+
+// Local async function to fetch the connection string and configure the application
+async Task ConfigureApp()
 {
-    var connectionString = GetConnectionStringFromParameterStore().Result;
-    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 29)));
-});
+    var connectionString = await GetConnectionStringFromParameterStore();
 
+    builder.Services.AddDbContext<OperationStackedContext>(options =>
+    {
+        options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 29)));
+    });
 
-var app = builder.Build();
-app.MapHealthChecks("/health");
+    app.MapHealthChecks("/health");
 
-using (var scope = app.Services.CreateScope())
-{
-    var dbContext = scope.ServiceProvider.GetRequiredService<OperationStackedContext>();
-    app.UseCors("MyPolicy");
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.UseHttpsRedirection();
-    app.MapControllers();
-    await dbContext.Database.EnsureCreatedAsync();
-    app.Run();
+    using (var scope = app.Services.CreateScope())
+    {
+        var dbContext = scope.ServiceProvider.GetRequiredService<OperationStackedContext>();
+        app.UseCors("MyPolicy");
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+        app.MapControllers();
+        await dbContext.Database.EnsureCreatedAsync();
+    }
 }
 
+await ConfigureApp(); // Call the local async function
+app.Run();
 
- async Task<string> GetConnectionStringFromParameterStore()
+async Task<string> GetConnectionStringFromParameterStore()
 {
     var client = new AmazonSimpleSystemsManagementClient(Amazon.RegionEndpoint.EUWest1);
     var request = new GetParameterRequest
     {
         Name = "/operationstacked/connectionstring/",
-        WithDecryption = true
+        WithDecryption = true 
     };
 
     var response = await client.GetParameterAsync(request);
     return response.Parameter.Value;
+
+    //return "server=terraform-20230416162907179900000002.cat7knapsqxr.eu-west-1.rds.amazonaws.com:3306;Port=3306;Database=OperationStacked;User Id=operationstacked;Password=your_secure_password;";
 }
 
 
