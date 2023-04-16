14fcc462f5cf27c3c085269ab2224f46e46c399c
Joe Richardson <joeyrichardson96@gmail.com>
2023-04-17 00:59:50 +0100

    t

--- OperationStacked/Data/OperationStackedContext.cs
+++ OperationStacked/Data/OperationStackedContext.cs
@@ -1,18 +1,35 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Amazon.Runtime;
+using Amazon.SimpleSystemsManagement.Model;
+using Amazon.SimpleSystemsManagement;
+using Microsoft.EntityFrameworkCore;
 using OperationStacked.Entities;
+using Microsoft.Extensions.Options;
 
 namespace OperationStacked.Data
 {
     public class OperationStackedContext : DbContext
-{
-    public OperationStackedContext(DbContextOptions options) : base(options)
     {
+        private readonly string _connectionString;
+
+        public OperationStackedContext(DbContextOptions options, IOptions<ConnectionStringOptions> connectionStringOptions) : base(options)
+        {
+            _connectionString = connectionStringOptions.Value.ConnectionString;
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+                optionsBuilder.UseMySql(_connectionString, serverVersion, options => options.EnableRetryOnFailure());
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public virtual DbSet<LinearProgressionExercise> LinearProgressionExercises { get; set; }
+        public virtual DbSet<A2SHypertrophyExercise> A2SHypertrophyExercises { get; set; }
+        public virtual DbSet<Exercise> Exercises { get; set; }
+        public virtual DbSet<User> Users { get; set; }
     }
-
-    public virtual DbSet<LinearProgressionExercise> LinearProgressionExercises { get; set; }
-    public virtual DbSet<A2SHypertrophyExercise> A2SHypertrophyExercises { get; set; }
-    public virtual DbSet<Exercise> Exercises { get; set; }
-    public virtual DbSet<User> Users { get; set; }
-}
-
 }
--- OperationStacked/Program.cs
+++ OperationStacked/Program.cs
@@ -5,9 +5,16 @@ using OperationStacked.Extensions.ServiceExtensions;
 using Microsoft.EntityFrameworkCore;
 using Amazon.SimpleSystemsManagement;
 using Amazon.SimpleSystemsManagement.Model;
+using Amazon.Runtime;
+var connectionString = await GetConnectionStringFromParameterStore();
 
 var builder = WebApplication.CreateBuilder(args);
 var config = builder.Configuration;
+var connectionStrings = Environment.GetEnvironmentVariables();
+builder.Services.Configure<ConnectionStringOptions>(options =>
+{
+    options.ConnectionString = connectionString;
+});
 
 var tokenOptions = config.GetSection("TokenOptions").Get<TokenOptions>();
 var signingConfigurations = new SigningConfigurations(tokenOptions.Secret);
@@ -34,52 +41,47 @@ builder.Services.AddCors(options =>
     )
 );
 
-
-
-await using var app = builder.Build();
-
-// Local async function to fetch the connection string and configure the application
-async Task ConfigureApp()
+builder.Services.AddDbContext<OperationStackedContext>(options =>
 {
-    var connectionString = await GetConnectionStringFromParameterStore();
+    options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 29)));
+});
 
-    builder.Services.AddDbContext<OperationStackedContext>(options =>
-    {
-        options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 29)));
-    });
 
-    app.MapHealthChecks("/health");
+var app = builder.Build();
+app.MapHealthChecks("/health");
 
-    using (var scope = app.Services.CreateScope())
-    {
-        var dbContext = scope.ServiceProvider.GetRequiredService<OperationStackedContext>();
-        app.UseCors("MyPolicy");
-        app.UseSwagger();
-        app.UseSwaggerUI();
-        app.UseHttpsRedirection();
-        app.MapControllers();
-        await dbContext.Database.EnsureCreatedAsync();
-    }
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<OperationStackedContext>();
+    app.UseCors("MyPolicy");
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    await dbContext.Database.EnsureCreatedAsync();
+    app.Run();
 }
 
-await ConfigureApp(); // Call the local async function
-app.Run();
+
 
 async Task<string> GetConnectionStringFromParameterStore()
 {
-    var client = new AmazonSimpleSystemsManagementClient(Amazon.RegionEndpoint.EUWest1);
+    var awsCredentials = new BasicAWSCredentials("AKIARSRO64ZCHOPL376V", "VI28IOUpT5HMrb1k6R2ynSTjC/uhQaqMspK/7v5z");
+    var client = new AmazonSimpleSystemsManagementClient(awsCredentials, Amazon.RegionEndpoint.EUWest1);
     var request = new GetParameterRequest
     {
-        Name = "/operationstacked/connectionstring/",
-        WithDecryption = true 
+        Name = "operationstacked-connectionstring",
+        WithDecryption = true
     };
 
     var response = await client.GetParameterAsync(request);
     return response.Parameter.Value;
-
-    //return "server=terraform-20230416162907179900000002.cat7knapsqxr.eu-west-1.rds.amazonaws.com:3306;Port=3306;Database=OperationStacked;User Id=operationstacked;Password=your_secure_password;";
 }
 
 
 public partial class Program { }
 
+public class ConnectionStringOptions
+{
+    public string ConnectionString { get; set; }
+}
