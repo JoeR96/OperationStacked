2053a8919eb3fec73996ffe5ee70b4720fe6fa24
Joe Richardson <joeyrichardson96@gmail.com>
2023-04-16 20:25:58 +0100

    name

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -125,7 +125,7 @@ resource "aws_security_group" "ecs_security_group" {
 }
 
 resource "aws_lb_target_group" "operation_stacked_tg" {
-  name_prefix    = "operation-stacked-tg"
+  name    = "operation-stacked-tg"
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.operation_stacked_vpc.id
