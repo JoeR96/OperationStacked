dcba97ba798e274817318918bc31d100a35a0ac1
Joe Richardson <joeyrichardson96@gmail.com>
2023-04-16 18:47:12 +0100

    name prefix

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -39,6 +39,25 @@ resource "aws_ecs_task_definition" "operation_stacked_api" {
 
 resource "aws_iam_role" "execution_role" {
   name = "ecs_execution_role"
+  name_prefix = "ecs_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "task_role" {
+  name = "ecs_task_role"
+  name_prefix = "ecs_task_role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
