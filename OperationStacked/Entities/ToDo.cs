52d4b8c488fe8f72d9283c53cc9d039917459744
Joe Richardson <joeyrichardson96@gmail.com>
2023-05-17 23:21:39 +0100

    Remove entity

--- OperationStacked/Entities/ToDo.cs
+++ OperationStacked/Entities/ToDo.cs
@@ -9,7 +9,6 @@ namespace OperationStacked.Entities
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Category { get; set; }
         public string Description { get; set; }
         public string Username { get; set; }
         public bool Completed { get; set; }
