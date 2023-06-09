40f6986deb956a91f92236bf0389112d19611431
Joe Richardson <joeyrichardson96@gmail.com>
2023-06-09 19:19:00 +0100

    commit

---
+++ OperationStacked/Requests/UpdateWeekAndDayRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace OperationStacked.Requests
+{
+    public class UpdateWeekAndDayRequest
+    {
+        public int Week { get; set; }
+        public int Day { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
