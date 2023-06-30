dd3040597f157f57492c503176407d4695107b6c
Joe Richardson <joeyrichardson96@gmail.com>
2023-07-01 00:33:44 +0100

    Weeks and day

--- OperationStacked/Entities/RecipeIngredient.cs
+++ OperationStacked/Entities/RecipeIngredient.cs
@@ -7,5 +7,9 @@ public class Ingredient
 {
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    public Guid Id { get; set; }
+    public Guid UserId { get; set; }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public string Measurement { get; set; }
 }
