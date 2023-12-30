381337ad371c0469a2f97b4b72f947f826f25a3c
Joe Richardson <joeyrichardson96@gmail.com>
2023-12-30 22:24:21 +0000

    tf

---
+++ OperationStacked/Controllers/VersionController.cs
@@ -0,0 +1,21 @@
+﻿namespace OperationStacked.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class VersionController : ControllerBase
+{
+    private readonly string _appVersion;
+
+    public VersionController(IConfiguration configuration)
+    {
+        _appVersion = configuration["AppVersion"];
+    }
+
+    [HttpGet]
+    public IActionResult GetVersion()
+    {
+        return Ok(_appVersion);
+    }
+}
